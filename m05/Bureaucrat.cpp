74b02a23a9d00be227ab5bbad975a298a44140cb
Maggie Dulcie <mdulcie@ev-f1.kzn.21-school.ru>
2021-12-11 23:10:08 +0300

    ex 5 start hpp

---
+++ m05/Bureaucrat.cpp
@@ -0,0 +1,42 @@
+#include "Bureaucrat.hpp"
+
+/******************************************************************************/
+/* Constructors */
+
+//      init
+Bureaucrat::Bureaucrat(const int &grade)
+{}
+
+Bureaucrat::Bureaucrat(const std::string &name, const int &grade)
+{}
+
+//      copy
+
+Bureaucrat::Bureaucrat(const Bureaucrat &obj)
+{}
+
+/******************************************************************************/
+/* Destructors */
+
+Bureaucrat::~Bureaucrat()
+{}
+
+/******************************************************************************/
+/* Operators */
+
+//      =
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &obj)
+{}
+
+/******************************************************************************/
+/* Private functions */
+
+/******************************************************************************/
+/* Public functions */
+
+        /* Set atributs */
+
+        /* Get and show atributs */
+
+        /* other methods */
